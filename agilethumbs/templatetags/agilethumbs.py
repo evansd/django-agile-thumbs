43a449a7c6b4f51691f1219b6935e8c021ac3253
Mike Brear <mike@swerveconcepts.com>
2012-11-26 12:21:31 +0000

    bug fix

--- agilethumbs/templatetags/agilethumbs.py
+++
@@ -1,8 +0,0 @@
-from django import template
-from .. import image_url as get_image_url
-
-register = template.Library()
-
-@register.simple_tag
-def image_url(image, style='default'):
-    return get_image_url(image, style)
--- agilethumbs/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-from escape import *
-from object_to_id import *
-from simple_resize import *
-from image_request import *
